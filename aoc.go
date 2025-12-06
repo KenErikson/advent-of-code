d03ef6029e1feabc2e8b153b0921a5fc10791709
Ken Erikson <35490395+KenErikson@users.noreply.github.com>
2025-12-07 00:43:37 +0200

    Done setup

--- aoc.go
+++ aoc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -18,16 +19,17 @@ func main() {
 	}
 	runMode, year, day, part, inputMode := parseArgs(os.Args[1:])
 
-	println(runMode)
-	println(year)
-	println(day)
-	println(part)
-	println(inputMode)
-	
-	switch runMode{
+	println("RunMode", runMode)
+	println("Year", year)
+	println("Day", day)
+	println("Part", part)
+	println("InputMode", inputMode)
+
+	switch runMode {
 	case NormalRunMode:
+		run(strconv.Itoa(year), strconv.Itoa(day), strconv.Itoa(part), inputMode)
 	case InitRunMode:
-		initDay(year,day)
+		initDay(year, day)
 	default:
 		End("Dunno")
 	}
@@ -46,29 +48,74 @@ func printUsage() {
 	os.Exit(1)
 }
 
-func initDay(year int, day int){
-	targetDirectory := filepath.Join(fmt.Sprintf("%d",year), fmt.Sprintf("day%d",day))
-	if createErr := os.MkdirAll(targetDirectory, 0755); createErr != nil {
-		panic(createErr)
+func run(year string, day string, part string, inputMode InputMode) {
+	dirPath := filepath.Join(year, "day"+day)
+	goPath := filepath.Join(dirPath, "day"+day+"-" + part + ".go")
+	args := []string{"run", goPath, dirPath}
+	if inputMode == FullInputMode {
+		args = append(args, "full")
+	}
+	cmd := exec.Command("go", args... )
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	println(" - " + strings.Join(cmd.Args, " "))
+
+	err := cmd.Run()
+
+    if err != nil {
+        fmt.Println("Error:", err)
     }
-
-	inputFileExample := filepath.Join(targetDirectory,"input_example.txt")
-	inputFileFull := filepath.Join(targetDirectory,"input_full.txt")
-	part1File := filepath.Join(targetDirectory,fmt.Sprintf("day%d-1.go", day))
-	part2File := filepath.Join(targetDirectory,fmt.Sprintf("day%d-2.go", day))
-
-	createFile(inputFileExample)
-	createFile(inputFileFull)
-	createFile(part1File)
-	createFile(part2File)
 }
 
-func createFile( fileToCreate string) {
-	destinationFile, createDestErr := os.Create(fileToCreate)
-    if createDestErr != nil {
-        panic(createDestErr)
-    }
-    defer destinationFile.Close()
+func initDay(year int, day int) {
+	targetDirectory := filepath.Join(fmt.Sprintf("%d", year), fmt.Sprintf("day%d", day))
+	if createErr := os.MkdirAll(targetDirectory, 0755); createErr != nil {
+		panic(createErr)
+	}
+
+	inputFileExample := filepath.Join(targetDirectory, "input_example.txt")
+	inputFileFull := filepath.Join(targetDirectory, "input_full.txt")
+	part1File := filepath.Join(targetDirectory, fmt.Sprintf("day%d-1.go", day))
+	part2File := filepath.Join(targetDirectory, fmt.Sprintf("day%d-2.go", day))
+
+	contentBytes, readErr := os.ReadFile(filepath.Join("common", "setup", "dayExample.go.example"))
+	if readErr != nil {
+		panic(readErr)
+	}
+
+	dayExampleString := string(contentBytes)
+
+	part1Content := strings.ReplaceAll(dayExampleString, "$DAY$", strconv.Itoa(day))
+	part1Content = strings.ReplaceAll(part1Content, "$PART$", "1")
+	part1Content = strings.ReplaceAll(part1Content, "$YEAR$", strconv.Itoa(year))
+	part2Content := strings.ReplaceAll(dayExampleString, "$DAY$", strconv.Itoa(day))
+	part2Content = strings.ReplaceAll(part2Content, "$PART$", "2")
+	part2Content = strings.ReplaceAll(part2Content, "$YEAR$", strconv.Itoa(year))
+
+	createFile(inputFileExample, "")
+	createFile(inputFileFull, "")
+	createFile(part1File, part1Content)
+	createFile(part2File, part2Content)
+
+}
+
+func createFile(fileToCreate string, content string) {
+	destinationFile, createDestErr := os.OpenFile( // Open -> create, fail if exists
+		fileToCreate,
+		os.O_CREATE|os.O_EXCL|os.O_WRONLY,
+		0644,
+	)
+	if createDestErr != nil {
+		panic(createDestErr)
+	}
+	defer destinationFile.Close()
+
+	_, writeErr := destinationFile.WriteString(content)
+	if writeErr != nil {
+		panic(writeErr)
+	}
 }
 
 func getYearArgOrCurrentAocYear(intArgs []int) (intArgsOut []int, year int) {
@@ -94,14 +141,14 @@ func parseArgs(args []string) (runMode RunMode, year int, day int, part int, inp
 	part = 1
 
 	day = intArgs[0]
-	if day > 31 || day < 1{
+	if day > 31 || day < 1 {
 		End(fmt.Sprintf("Day is not as expected %d", day))
 	}
 	switch {
 	case len(intArgs) == 1:
 	case len(intArgs) == 2:
 		part = intArgs[1]
-		if part != 1 && part != 2{
+		if part != 1 && part != 2 {
 			End(fmt.Sprintf("Part is not 1 or 2: %d", part))
 		}
 	default:
--- common/util.go
+++ common/util.go
@@ -1,6 +1,11 @@
 package common
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func End(msg string, errs ...error){
 	println("")
@@ -15,4 +20,35 @@ func End(msg string, errs ...error){
 	println("")
 	println("Exiting")
 	os.Exit(1)
+}
+
+func ParseInputFile( args []string )(stringData string, stringDataRows []string, rows int ){
+	targetDir := args[1]
+	
+	inputFile := "input_example.txt"
+	if len(args) > 2 {
+		if args[2] == "full" {
+			inputFile = "input_full.txt"
+		}
+	}
+
+	fmt.Printf(" - %s \n", inputFile)
+
+	inputFile = filepath.Join(targetDir, inputFile)
+
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Println("Can't read file:", inputFile)
+		panic(err)
+	}
+
+	stringData = string(data)
+	stringDataRows = strings.Split(stringData, "\n")
+	rows = len(stringDataRows)
+
+	fmt.Println("Read ", inputFile, " with ", rows, " rows. First row:")
+	println(stringDataRows[0])
+	println()
+
+	return
 }
