70fb908387ac956e25c331db67c60e7ed2fbaf12
Ken Erikson <ken@hibox.fi>
2025-12-07 11:18:27 +0200

    2025 day 3 part 1

--- 2025/day3/day3-1.go
+++ 2025/day3/day3-1.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"os"
 	_ "sort"
+	"strconv"
 	_ "strconv"
 	_ "strings"
+
 	. "aoc.erikson.fi/common"
 )
 
@@ -15,8 +17,27 @@ func main() {
 	fmt.Println("\n### Day 3 - Part 1 ###")
 
 	stringData, stringDataRows, rows := ParseInputFile(os.Args)
-	
+
 	_, _, _ = stringData, stringDataRows, rows
 
-	println("Result: ", )
+	joltage := 0
+	for _, row  := range stringDataRows {
+		println(row)
+		maxJoltage := 0
+		for i := 0; i < len(row) - 1; i++ {
+			valI, _ := strconv.Atoi(row[i:i+1])
+			for j := i+1; j < len(row); j++ {
+				valJ, _ := strconv.Atoi(row[j:j+1])
+				possibleMaxJoltage, _ := strconv.Atoi(fmt.Sprintf("%d%d", valI, valJ))
+				if possibleMaxJoltage > maxJoltage{
+					println("New max joltage: ", possibleMaxJoltage)
+					maxJoltage = possibleMaxJoltage
+				}
+			}
+		}
+		println("Max Joltage: ", maxJoltage)
+		joltage += maxJoltage
+	}
+
+	println("Result: ", joltage)
 }
