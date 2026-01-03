2b4ff55ee6200ab0a69fb3f5099691acf1605dfe
Ken Erikson <35490395+KenErikson@users.noreply.github.com>
2026-01-04 00:19:28 +0200

    Setup

--- 2025/day12/part1.swift
+++ 2025/day12/part1.swift
@@ -9,11 +9,11 @@ let content = try String(contentsOfFile: filePath, encoding: String.Encoding.utf
 
 let contentGroups = content.split(separator: "\n\n").map(String.init)
 
-guard let layouts = contentGroups.last else {
-    exit(1)
-}
+let layouts = contentGroups.last!
 
 var shapes: [[[Character]]] = [[[]], [[]], [[]], [[]], [[]], [[]]]
+
+var shapeCounts: [Int] = []
 for (i, group) in contentGroups.enumerated() {
     if i > 5 {
         break
@@ -26,12 +26,42 @@ for (i, group) in contentGroups.enumerated() {
         if j > 1 {
             shapes[i].append([])
         }
+        var hashCount = 0
         for chr in line {
             // print("i", i, "j", j )
             // print(shapes)
             shapes[i][j - 1].append(chr)
+            if chr == "#" {
+                hashCount += 1
+            }
         }
+        shapeCounts.append(hashCount)
     }
 }
 
 print(shapes)
+
+// print(layouts)
+
+var successCount = 0
+for layout in layouts.split(separator: "\n").map(String.init) {
+    let parts = layout.split(separator: ":")
+
+    let areaString = parts[0]
+    let height = Int(areaString.split(separator: "x")[0])!
+    let width = Int(areaString.split(separator: "x")[1])!
+
+    let shapeCounts = parts[1].split(separator: " ").compactMap({ a in Int(a)! })
+
+    if canFit(width, height, shapeCounts) {
+        successCount += 1
+    }
+}
+
+print("Res", successCount)
+
+func canFit(_ width: Int, _ height: Int, _ shapeCounts: [Int]) -> Bool {
+    var matrix: [[Character]] = Array(repeating: Array(repeating: ".", count: width), count: height)
+
+    return false
+}
